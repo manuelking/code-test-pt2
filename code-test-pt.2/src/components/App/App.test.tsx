5e9350838da1365be9d2757b19872dab8eda018a
emdev <42058287+manuelking@users.noreply.github.com>
2023-11-26 18:10:59 +0000

    Update App.test.tsx

--- code-test-pt.2/src/components/App/App.test.tsx
+++ code-test-pt.2/src/components/App/App.test.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App, OrderItem } from './App'
-import { Order } from '../Order/Order'
-import { Product } from '../../models/Product'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { App, OrderItem } from './App'
+import '@testing-library/jest-dom/extend-expect'
+import { Order } from '../Order/Order'
 
-const productA = new Product('CA6', 'Cake', 200)
-
-const mockOrder = [
-  {
-    ...productA,
-  },
-]
+const productA = {
+  code: 'CA6',
+  name: 'Cake',
+  price: 200,
+}
 
 const orderItems = [
   {
@@ -40,4 +38,51 @@ test('handleAddProduct adds product to order', () => {
   )
 
   userEvent.click(screen.getByTestId('add-CA6'))
+})
+
+test('adds a product to the order on clicking', () => {
+  render(<App />)
+  fireEvent.click(screen.getByTestId(productA.code))
+
+  const orderItem = screen.getByTestId(`qty-${productA.code}`)
+  expect(orderItem).toHaveTextContent('Qty:1')
+})
+
+test('removes a product from the order', () => {
+  render(<App />)
+  userEvent.click(screen.getByTestId(productA.code))
+  userEvent.click(screen.getByTestId(`remove-${productA.code}`))
+
+  const orderItem = screen.queryByTestId(`qty-${productA.code}`)
+  expect(orderItem).toBeNull()
+})
+
+test('increments quantity on clicking add', () => {
+  render(<App />)
+  userEvent.click(screen.getByTestId(productA.code))
+  userEvent.click(screen.getByTestId(`add-${productA.code}`))
+
+  const orderItem = screen.getByTestId(`qty-${productA.code}`)
+  expect(orderItem).toHaveTextContent('Qty:2')
+})
+
+test('decrements quantity on clicking decrement', () => {
+  render(<App />)
+  userEvent.click(screen.getByTestId(productA.code))
+  userEvent.click(screen.getByTestId(`decrement-${productA.code}`))
+
+  const orderItem = screen.queryByTestId(`qty-${productA.code}`)
+  expect(orderItem).toBeNull()
+})
+test('updates quantity on clicking decrement and increment buttons', () => {
+  render(<App />)
+  userEvent.click(screen.getByTestId(productA.code))
+
+  userEvent.click(screen.getByTestId(`add-${productA.code}`))
+  let orderItem = screen.getByTestId(`qty-${productA.code}`)
+  expect(orderItem).toHaveTextContent('Qty:2')
+
+  userEvent.click(screen.getByTestId(`decrement-${productA.code}`))
+  orderItem = screen.getByTestId(`qty-${productA.code}`)
+  expect(orderItem).toHaveTextContent('Qty:1')
 })
