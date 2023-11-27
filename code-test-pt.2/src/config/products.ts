1e7ca58ec8139fa364da936e450d224d682e019c
emdev <42058287+manuelking@users.noreply.github.com>
2023-11-27 13:04:20 +0000

    Delete code-test-pt.2 directory

--- code-test-pt.2/src/config/products.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../models/Product";
-
-export const productData = [
-  new Product("CA6", "Cake", 200),
-  new Product("A21", "Kitty Litter", 1899),
-  new Product("G95", "Asparagus", 83),
-];
